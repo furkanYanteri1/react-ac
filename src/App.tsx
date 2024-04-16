7e4e47879e38912606e8573979258ec12d79db8b
Furkan Yanteri <furkan.yanteri@pitcher.com>
2024-04-16 19:06:42 +0300

    init: ready to go

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <div className='searchBar'> searchBar</div>
+        <div className='searchResults'> searchResults</div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
